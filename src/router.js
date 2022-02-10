08c5dd5b61682e59c1b67ed33e319aacb1af97f2
dhruvi-wohlig <97166039+dhruvi-wohlig@users.noreply.github.com>
2022-02-10 17:05:19 +0530

    Updated roter.js

--- src/router.js
+++ src/router.js
@@ -1,9 +1,23 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 import Register from './components/RegisterForm.vue'
 import Login from './components/Login.vue'
+import Home from './components/Home.vue'
+import SuccessPage from './components/SuccessPage.vue'
 
-export default ({
-    routes: [
-        {path: '/', component: Register},
-        {path: '/login', component: Login}
-    ]
-})
+Vue.use(VueRouter);
+
+const routes = [
+      {path: '/', component: Home},
+      {path: '/login', component: Login},
+      {path: '/register', component: Register},
+      {path: '/successfulPage', component: SuccessPage},
+      {path: '/notFound(.*)', redirect: '/' }
+];
+
+const router = new VueRouter({
+    mode: "history",
+    routes,
+  });
+
+export default router;
